1aae42d857ff5a8060ca57c31aac12e4d9b6b2a1
bastiion <graviola@posteo.de>
2019-06-25 13:08:18 +0200

    cleanup comments

--- plantain/shared/src/main/scala/org/w3/banana/plantain/model/IntHexastoreGraph.scala
+++ plantain/shared/src/main/scala/org/w3/banana/plantain/model/IntHexastoreGraph.scala
@@ -2,11 +2,6 @@ package org.w3.banana.plantain.model
 
 
 case class IntHexastoreGraph[S, P, O](hexaTriples: HexastoreTriples[Int], dicts: SPODictionary[Int, S, P, O], size: Int) extends HexastoreGraph[Int, S, P, O] {
-  //override def size: Int = _size
-
-  //override def hexaTriples: HexastoreTriples[Int] = _hexaTriples
-
-  //override def dicts: SPODictionaries[Int, S, P, O] = _dicts
 
   override def -(subject: S, predicate: P, objectt: O): IntHexastoreGraph[S, P, O] = super.-(subject, predicate, objectt).asInstanceOf[IntHexastoreGraph[S, P, O]]
 
